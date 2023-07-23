8b36d223e82e813ac9f3f79a8861b8163aaa656b
Kunal Kumar Sahoo <kunal.sahoo2003@gmail.com>
2023-07-23 10:32:57 +0530

    Added functionalities for Layer class

--- NeuralNetwork.cpp
+++ NeuralNetwork.cpp
@@ -35,3 +35,15 @@ void Neuron::transfer() {
 }
 
 
+Layer::Layer(int n_neurons, int n_weights) {
+    this->initNeurons(n_neurons, n_weights);
+}
+
+Layer::~Layer() {}
+
+void Layer::initNeurons(int n_neurons, int n_weights) {
+    for(int i=0; i<n_neurons; ++i)
+        m_neurons.push_back(Neuron(n_weights));
+}
+
+
